03deb3461ed4f229a4070c8b9a1e1b02db7822cb
limeunha <eh1501@naver.com>
2024-12-13 18:12:02 +0900

    up

---
+++ board-api/models/board.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize')
+
+module.exports = class Board extends Sequelize.Model {
+   static init(sequelize) {
+      return super.init(
+         {
+            //글내용
+            content: {
+               type: Sequelize.TEXT,
+               allowNull: false,
+            },
+            //이미지 경로 및 파일명
+            img: {
+               type: Sequelize.STRING(200),
+               allowNull: true,
+            },
+         },
+         {
+            sequelize,
+            timestamps: true,
+            underscored: false,
+            modelName: 'Board',
+            tableName: 'Boards',
+            paranoid: true,
+            charset: 'utf8mb4',
+            collate: 'utf8mb4_general_ci',
+         }
+      )
+   }
+   static associate(db) {
+      db.Board.belongsTo(db.User)
+   }
+}
--- board-api/models/index.js
+++ board-api/models/index.js
@@ -3,15 +3,19 @@ const env = process.env.NODE_ENV || 'development'
 const config = require('../config/config')[env]
 
 const User = require('./user')
+const Board = require('./board')
 
 const db = {}
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
 
 db.sequelize = sequelize
 db.User = User
+db.Board = Board
 
 User.init(sequelize)
+Board.init(sequelize)
 
 User.associate(db)
+Board.associate(db)
 
 module.exports = db
